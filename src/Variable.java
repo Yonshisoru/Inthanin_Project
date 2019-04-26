e1af0e341b4caf50a0c731aedc812ad3f94cb19c
Yonshisoru <44347209+Yonshisoru@users.noreply.github.com>
2019-04-27 01:58:17 +0700

    Update Variable.java
    Adding Comment Variable.java

--- src/Variable.java
+++ src/Variable.java
@@ -14,36 +14,23 @@ import javax.swing.JOptionPane;
  * @author Yonshisoru
  */
 public class Variable {
-    MongoClient mongo;
-    DB db;
-    DBCollection DBC;
-    static public int id;
-    static public int status;
+//--------------------------MongoDB variable-------------------------------
+    MongoClient mongo; //กำหนดตัวแปรประเภท MongoClient
+    DB db; //กำหนดตัวแปรประเภท DB
+    DBCollection DBC; //กำหนดตัวแปรประเภท DBCollection
+    static public int id; //ัตัวแปรรหัสของพนักงานแบบ static
+    static public int status; //ตัวแปรสถานะของพนักงานแบบ static
     
-    public DB getConnect(){
-    try{
-        MongoClient mongo = new MongoClient("localhost",27017);
-        db = mongo.getDB("InthaninDB");
-        System.out.println(db);
-        System.out.println(mongo.getConnectPoint());
-    }catch(Exception e){
+    public void getConnect(){ //ฟังก์ชั่นการเชื่อมต่อ
+    try{ //ดักจับการทำงานผิดพลาดโดยใช้ try-catch
+        MongoClient mongo = new MongoClient("localhost",27017);//เชื่อมต่อ Database Mongodb IP:localhost Port:27017
+        db = mongo.getDB("InthaninDB");//ดึงข้อมูลฐานข้อมูล
+    }catch(Exception e){ //ดักจับการทำงานผิดพลาดทุกอย่างโดยให้ชื่อว่า e
+            e.printStackTrace(); //แสดงออกการผิดพลาดทางหน้าจอ
     }
-    return db;
 }
-    public int getid(){
-        return this.id;
-    }
-    public int getstatus(){
-        return this.status;
-    }
-    public void setid(int id){
-        this.id = id;
-    }
-    public void setstatus(int status){
-        this.status = status;
-    }
-    public String month(int month){
-    String output="";
+    public String month(int month){ //ฟังก์ชั่นเปลี่ยนลำดับเดือนเป็นชื่อเดือน
+    String output=""; //ตัวแปร Stringเพื่อใช้เก็บผลลัพธ์
     switch(month){
         case 1:output="January";break;
         case 2:output="Febuary";break;
@@ -58,6 +45,6 @@ public class Variable {
         case 11:output="November";break;
         case 12:output="December";break;
     }
-    return output;
+    return output; //แสดงผลลัพธ์
 }
 }
